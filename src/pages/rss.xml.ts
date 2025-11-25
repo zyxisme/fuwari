eb9e892ea9cea1db4c06c8bd6068cb1372af1c16
zyx <103358586+zyxisme@users.noreply.github.com>
2025-11-25 13:38:53 +0800

    Update stripInvalidXmlChars function
    Fix regex to strip invalid XML characters and remove img tags.

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -9,6 +9,7 @@ import { siteConfig } from "@/config";
 const parser = new MarkdownIt();
 
 function stripInvalidXmlChars(str: string): string {
+	str.replace("<img *>", "");
 	return str.replace(
 		// biome-ignore lint/suspicious/noControlCharactersInRegex: https://www.w3.org/TR/xml/#charsets
 		/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F-\x9F\uFDD0-\uFDEF\uFFFE\uFFFF]/g,
