2d2425062d04c1ceb9115280460a7206196bb011
zyx <103358586+zyxisme@users.noreply.github.com>
2025-11-30 21:45:51 +0800

    Remove AUTO_MODE from light/dark mode sequence

--- src/components/LightDarkSwitch.svelte
+++ src/components/LightDarkSwitch.svelte
@@ -11,7 +11,7 @@ import {
 import { onMount } from "svelte";
 import type { LIGHT_DARK_MODE } from "@/types/config.ts";
 
-const seq: LIGHT_DARK_MODE[] = [LIGHT_MODE, DARK_MODE, AUTO_MODE];
+const seq: LIGHT_DARK_MODE[] = [LIGHT_MODE, DARK_MODE];
 let mode: LIGHT_DARK_MODE = $state(AUTO_MODE);
 
 onMount(() => {
